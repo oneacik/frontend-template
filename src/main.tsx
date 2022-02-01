0a04ea6ee79f7ed4681efc9763c9148ebbf5b552
psuwala <oneat@vp.pl>
2022-02-01 12:46:39 +0100

    fixed definition of webpack

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,1 @@
-alert("ksi");
+alert("ksidelta");
--- webpack.config.js
+++ webpack.config.js
@@ -4,36 +4,41 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
-  entry: './src/main.tsx',
-  devtool: 'eval-source-map',
-  mode: 'development',
-  target: 'web',
-  output: {
-    path: path.join(__dirname, '/public/script'),
-    filename: 'bundle.js',
-    publicPath: '/script/'
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.jsx']
-  },
-  devServer: {
-    port: 9000 
-  },
-  module: {
-    rules: [
-      {
-        test: /\.[tj]sx?$/,
-        exclude:path.resolve(__dirname, "node_modules"),
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.(css)$/,
-        use: [
-          'style-loader',
-          'css-loader'
+    entry: './src/main.tsx',
+    devtool: 'eval-source-map',
+    mode: 'development',
+    target: 'web',
+    output: {
+        path: path.join(__dirname, '/public'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.jsx']
+    },
+    devServer: {
+        port: 9000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: path.resolve(__dirname, "node_modules"),
+                loader: 'ts-loader'
+            },
+            {
+                test: /\.(css)$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
         ]
-      }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Some page",
+            template: "assets/index.html",
+            inject: false
+        })
     ]
-  },
-  plugins: [new HtmlWebpackPlugin()]
 }
